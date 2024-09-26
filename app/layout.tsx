b8425fb80a003206138cc00287f80a004e05b3e7
Marvel.Codes <marvellousn561@gmail.com>
2024-09-26 11:52:31 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './provider';
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
+import { Analytics } from "@vercel/analytics/react"
 
 import './globals.css';
 import Script from 'next/script';
@@ -24,19 +25,9 @@ export default function RootLayout({
         src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6171078836003402'
         crossOrigin='anonymous'
       />
-      {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5LT6F846');
-            `,
-          }}
-        /> */}
-        <GoogleAnalytics gaId='G-5LT6F846' />
-
+      <GoogleAnalytics gaId='G-5LT6F846' />
+      <Analytics/>
+      
       <body className={inter.className}>
         <GoogleTagManager gtmId='GTM-5LT6F846' />
         <Providers>{children}</Providers>
